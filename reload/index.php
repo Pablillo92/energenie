392aa2893b5fbfbc54154d0f8eb3f1e198aee7ea
Pablo Sáez Guerrero <pablo@intelligenia.com>
2016-05-03 12:53:16 +0200

    archivos de cambios de ip

--- reload/index.php
+++ reload/index.php
@@ -1,37 +1,9 @@
 <?php
-/*
-if($_SERVER["HTTP_X_FORWARDED_FOR"]) { 
-    if($pos=strpos($_SERVER["HTTP_X_FORWARDED_FOR"]," ")) 
-    { 
-        echo "IP local: ".substr($_SERVER["HTTP_X_FORWARDED_FOR"],0,$pos)." - IP Pública: ".substr($_SERVER["HTTP_X_FORWARDED_FOR"],$pos+1); 
-        $hostlocal=substr($_SERVER["HTTP_X_FORWARDED_FOR"],$pos+1); 
-    }else{ 
-        echo "&ippublica=".$_SERVER["HTTP_X_FORWARDED_FOR"]; 
-        $hostlocal=$_SERVER["HTTP_X_FORWARDED_FOR"]; 
-    } 
-  
-}else{ 
-    echo "&ippublica=".$_SERVER["REMOTE_ADDR"]; 
-} 
 
 
-   if(isset($_SERVER['HTTP_X_FORWARDED_FOR'])) { 
-	   $ip = $_SERVER['HTTP_X_FORWARDED_FOR']; 
-   }elseif(isset($_SERVER['HTTP_VIA'])) { 
-	   $ip = $_SERVER['HTTP_VIA']; 
-   }   
-   elseif(isset($_SERVER['REMOTE_ADDR'])){
-	   $ip = $_SERVER['REMOTE_ADDR'];
-   } 
-   else{
-	   $ip = "Desconocido";
-   } 
-   echo "&ip=" . $ip ; 
-*/
 $postdata = http_build_query(
     array(
-        'id' => '13',
-        'ip' => '192.168.1.135'
+        'id' => '13'
     )
 );
 
